206eda2dfe6eb06acb31de852f5b698504d07c60
abhivarma <abhilash.dongari2@gmail.com>
2023-02-06 00:48:43 +0530

    Express Server Static Files

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express=require('express');
 
 const http=require('http');
+const morgan=require('morgan');
 
 const hostname='localhost';
 const port=3000;
 
 const app=express();
+app.use(morgan('dev'));
+app.use(express.static(__dirname+'/public'));
+
 
 app.use((req,res,next)=>{
-    console.log('req,hader');
     res.statusCode=200;
     res.setHeader('Content-Type','text/html');
     res.end('<html><body><h1>This is a Express Server</h1></body></html>');
@@ -18,7 +21,7 @@ app.use((req,res,next)=>{
 const server= http.createServer(app);
 
 server.listen(port,hostname, ()=>{
-    console.log(`Server starting at http://${hostname}:${port}`);
-    
+    console.log(`Server starting at http://${hostname}:${port}/`);
+
 });
 
